98ec7e1e49d1b0361f750bded580b9512b12552f
peppa <1039812805@qq.com>
2024-05-14 00:03:35 +0800

    项目依赖配置

---
+++ rpc-demo/rpc-api/src/main/java/com/peppa/api/Hello.java
@@ -0,0 +1,9 @@
+package com.peppa.api;
+
+/**
+ * @author peppa
+ * @time 2024-05-13 星期一 23:36
+ */
+public interface Hello {
+    String sayHello(String name);
+}
---
+++ rpc-demo/rpc-provider/src/main/java/com/peppa/apiImpl/HelloImpl.java
@@ -0,0 +1,10 @@
+package com.peppa.apiImpl;
+
+import com.peppa.api.Hello;
+
+public class HelloImpl implements Hello {
+    @Override
+    public String sayHello(String name) {
+        return "hello" + name;
+    }
+}
